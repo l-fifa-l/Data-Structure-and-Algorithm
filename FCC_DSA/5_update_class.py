8c230832fb78f7774203ca9c7d336e26674bc2b0
Vivek <starktestic@gmail.com>
2022-06-19 12:01:29 +0530

    Update 5_update_class.py

--- FCC_DSA/5_update_class.py
+++ FCC_DSA/5_update_class.py
@@ -1,16 +1,41 @@
 class User:
-    def __init__(self, name, email, username):
+    def __init__(self, name,  username, email):
         self.name = name
         self.email = email
         self.username = username
     print("User created")
 
-    def introduce_youself(self, guestname):
-        print(f"Hi {guestname} I'm {self.name}! contact me at {self.email}")
+    def __repr__(self):
+        return "User(username='{}',name='{}',email='{}')".format(self.username, self.name, self.email)
+
+    def __str__(self):
+        return self.__repr__()
 
 
-user1 = User("John", "jo@neja.com", "janeja")
+john = User("John", "jo", "jo@john.com")
+jane = User("Jane", "ja", "ja.jane.com")
+vivek = User("Vivek", "fifa", "vivek@fifa.com")
+vrinda = User("Vrinda", "bindu", "vrinda@bindu.com")
+tanvi = User("Tanvi", "tan", "tanvi@tan.com")
+kshitij = User("Kshitij", "harsh", "kshitij@harsh.com")
+sharad = User("Sharad", "build", "sharad@build.com")
 
-print(user1.name)
+users = [john, jane, vivek, vrinda, tanvi, kshitij, sharad]
 
-user1.introduce_youself("David")
+print(john.name, john.email, john.username)
+print(john)
+print(users)
+
+
+class UserDatabase:
+    def insert(self, user):
+        pass
+
+    def find(self, username):
+        pass
+
+    def update(self, user):
+        pass
+
+    def list_all(self):
+        pass
